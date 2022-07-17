0ec4782e0b2350b8377adb5cafee77940197b8f8
eovywjr1 <40791869+eovywjr1@users.noreply.github.com>
2022-07-18 00:18:21 +0900

    인벤토리 페이지 구현(0717)
    +카메라 구현 시도

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -5,42 +5,59 @@ using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour
 {
-    public List<GameObject> itemList, selectedList;
+    public List<GameObject> slotList, selectedList;
+    public List<Item> itemList;
     public GameObject selected3, selected2, hp;
-    public int moveIndex, selectedIndex;
+    public int moveIndex, selectedIndex, pageIndex = 0;
     public Sprite unmovedImage, movedImage, unselectedImage, selectedImage;
     public bool selectedItem;
 
     void Start()
     {
-        itemList[moveIndex].GetComponent<Image>().sprite = movedImage;
+        slotList[0].GetComponent<Image>().sprite = movedImage;
         hp.GetComponent<Transform>().SetAsFirstSibling();
+        ShowFirstPageItem();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w")) {
-            if (!selectedItem && moveIndex > 3) {
-            
-                moveIndex -= 4;
-                MovedIndex(itemList, moveIndex + 4, moveIndex, unmovedImage, movedImage);
+        Moved();
+        Selected();
+    }
+
+    void Moved()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            if (!selectedItem && moveIndex <= 3)
+            {
+                Debug.Log("페이지 선택");
             }
-            else if(selectedItem && selectedIndex != 0)
+            else if (!selectedItem && moveIndex > 3)
+            {
+                MovedIndex(slotList, (moveIndex % 12), (moveIndex - 4) % 12, unmovedImage, movedImage);
+                moveIndex -= 4;
+                ChangePageItem();
+            }
+            else if (selectedItem && selectedIndex != 0)
             {
                 selectedIndex--;
                 MovedIndex(selectedList, selectedIndex + 1, selectedIndex, unselectedImage, selectedImage);
             }
         }
-        if (Input.GetKeyDown("a") && moveIndex > 0 && !selectedItem)
+        else if (Input.GetKeyDown("a") && moveIndex > 0 && !selectedItem)
         {
+            MovedIndex(slotList, moveIndex % 12, (moveIndex - 1) % 12, unmovedImage, movedImage);
             moveIndex--;
-            MovedIndex(itemList, moveIndex + 1, moveIndex, unmovedImage, movedImage);
+            ChangePageItem();
         }
-        if (Input.GetKeyDown("s")) {
-            if (!selectedItem && itemList.Count > moveIndex + 4){
+        else if (Input.GetKeyDown("s"))
+        {
+            if (!selectedItem && itemList.Count > moveIndex + 4)
+            {
+                MovedIndex(slotList, moveIndex % 12, (moveIndex + 4) % 12, unmovedImage, movedImage);
                 moveIndex += 4;
-                MovedIndex(itemList, moveIndex - 4, moveIndex, unmovedImage, movedImage);
+                ChangePageItem();
             }
             else if (selectedItem && selectedIndex != 2)
             {
@@ -48,11 +65,16 @@ public class InventoryScript : MonoBehaviour
                 MovedIndex(selectedList, selectedIndex - 1, selectedIndex, unselectedImage, selectedImage);
             }
         }
-        if (Input.GetKeyDown("d") && itemList.Count > moveIndex + 1 && !selectedItem)
+        else if (Input.GetKeyDown("d") && itemList.Count > moveIndex + 1 && !selectedItem)
         {
+            MovedIndex(slotList, moveIndex % 12, (moveIndex + 1) % 12, unmovedImage, movedImage);
             moveIndex++;
-            MovedIndex(itemList, moveIndex - 1, moveIndex, unmovedImage, movedImage);
+            ChangePageItem();
         }
+    }
+
+    void Selected()
+    {
         if (Input.GetKeyDown("e"))
         {
             if (!selectedItem)
@@ -65,7 +87,7 @@ public class InventoryScript : MonoBehaviour
     void ActiveSelected()
     {
         selected3.SetActive(true);
-        selected3.transform.position = new Vector2(itemList[moveIndex].transform.position.x + 215, itemList[moveIndex].transform.position.y - 65);
+        selected3.transform.position = new Vector2(slotList[moveIndex].transform.position.x + 215, slotList[moveIndex].transform.position.y - 65);
         selectedItem = true;
         selectedList[0].GetComponent<Image>().sprite = selectedImage;
     }
@@ -82,5 +104,26 @@ public class InventoryScript : MonoBehaviour
     {
         list[preIndex].GetComponent<Image>().sprite = unImage;
         list[nextIndex].GetComponent<Image>().sprite = doImage;
+    }
+
+    void ShowFirstPageItem()
+    {
+        for (int i = 0; i < 12 && i < itemList.Count; i++)
+            slotList[i].transform.GetChild(0).GetComponent<InventorySlot>().item = itemList[i];
+    }
+
+    void ChangePageItem()
+    {
+        if(pageIndex != moveIndex / 12)
+        {
+            pageIndex = moveIndex / 12;
+
+            int i, j;
+            for (i = pageIndex * 12, j = 0; i < pageIndex * 12 + 12 && i < itemList.Count; i++, j++)
+                slotList[j].transform.GetChild(0).GetComponent<InventorySlot>().item = itemList[i];
+
+            for (; j < 12; j++)
+                slotList[j].transform.GetChild(0).GetComponent<InventorySlot>().item = null;
+        }
     }
 }
