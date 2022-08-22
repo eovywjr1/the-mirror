e5ae3ff70c75ffdea088e6944a88c2090deb0fc6
SeongHuiJung <tjdgml150237@naver.com>
2022-08-23 06:48:09 +0900

    타이틀 애니메이션 수정

--- Assets/Scripts/Title/TitleScript.cs
+++ Assets/Scripts/Title/TitleScript.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class TitleScript : MonoBehaviour
 {
+    public Animator startingAnim;
     public void StartGame()//게임 실행
     {
         Debug.Log("게임 실행");
+        startingAnim = GetComponent<Animator>();
+        startingAnim.SetBool("StartLoad", true);
     }
     public void ExitGame()//게임 종료
     {
